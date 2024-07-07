8af682eb386c2ba93734aab8d4da7d34a60404c0
JuaniSilva <juanicruzsilva@gmail.com>
2024-07-07 19:06:29 -0300

    feat: add base styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,26 @@
-import type { Config } from "tailwindcss";
+import { fontFamily } from 'tailwindcss/defaultTheme';
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        'primary-dark-blue': 'hls(233, 26%, 24%)',
+        'primary-lime-green': 'hls(136, 65%, 51%)',
+        'primary-bright-cyan': 'hls(192, 70%, 51%)',
+        'neutral-grayish-blue': 'hls(233, 8%, 62%)',
+        'neutral-light-grayish-blue': 'hls(220, 16%, 96%)',
+        'neutral-very-light-gray': 'hls(0, 0%, 98%)',
       },
+      fontFamily: {
+        'public-sans': ['var(--font-public-sans)', ...fontFamily.sans],
+      },
+      backgroundImage: {},
     },
   },
   plugins: [],
